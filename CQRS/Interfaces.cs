8bef6e99f5081b0e3a641b92b6bb9f193edf8a29
Benjamin Dickey <benjamindickey@hotmail.co.uk>
2023-03-05 16:40:20 +0000

    Add boilerplate CQRS chatroom app

---
+++ CQRS/Interfaces.cs
@@ -0,0 +1,71 @@
+﻿namespace CQRS;
+
+public interface IMediator
+{
+    TReturn Send<TQuery, TReturn>(TQuery query) where TQuery : IQuery<TReturn>;
+    void Send<TCommand>(TCommand command) where TCommand : ICommand;
+    void Register<TCommand>(ICommandHandler<TCommand> commandHandler) where TCommand : ICommand;
+    void Register<TQuery, TReturn>(IQueryHandler<TQuery, TReturn> commandHandler) where TQuery : IQuery<TReturn>;
+}
+
+public interface IMessageWriter
+{
+    void Write(IChatRoom chatRoom, ChatMessage message);
+}
+
+#region Chatroom
+public interface IParticipant
+{
+    string Name { get; }
+    void Join(IChatRoom chatRoom);
+    void Leave(IChatRoom chatRoom);
+    void SendMessageTo(IChatRoom chatRoom, string message);
+    void NewMessageReceivedFrom(IChatRoom chatRoom, ChatMessage message);
+    IEnumerable<IParticipant> ListParticipantsOf(IChatRoom chatRoom);
+    IEnumerable<ChatMessage> ListMessagesOf(IChatRoom chatRoom);
+}
+
+public interface IChatRoom
+{
+    string Name { get; }
+
+    // Participant related
+    void Add(IParticipant participant);
+    void Remove(IParticipant participant);
+    IEnumerable<IParticipant> ListParticipants();
+
+    // Message related
+    void Add(ChatMessage message);
+    IEnumerable<ChatMessage> ListMessages();
+}
+#endregion
+
+#region Command & Query
+
+/// <summary>
+/// Command: Something which changes state of app
+/// </summary>
+public interface ICommand { }
+/// <summary>
+/// Ensures implementer is set up to handle any Command
+/// </summary>
+public interface ICommandHandler<TCommand> where TCommand : ICommand
+{
+    void Handle(TCommand command);
+}
+
+/// <summary>
+/// Query: Something which reads the state of the app and doesn't change anything
+/// </summary>
+/// <typeparam name="TReturn">Some type of state information</typeparam>
+public interface IQuery<TReturn> { }
+/// <summary>
+/// Ensures implementer is set up to handle any Query which returns something
+/// </summary>
+/// <typeparam name="TQuery">Type of query to take in</typeparam>
+/// <typeparam name="TReturn">Type of data to return</typeparam>
+public interface IQueryHandler<TQuery, TReturn> where TQuery : IQuery<TReturn>
+{
+    TReturn Handle(TQuery query);
+}
+#endregion
